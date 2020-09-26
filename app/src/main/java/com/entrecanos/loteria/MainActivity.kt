422c896f2baae762157eb22081f657e7c3972bf3
Whatevaaar <emilianohidalgo314@gmail.com>
2020-09-26 12:21:34 -0500

    fin

--- app/src/main/java/com/entrecanos/loteria/MainActivity.kt
+++ app/src/main/java/com/entrecanos/loteria/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.entrecanos.loteria
 
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.stripe.stripeterminal.TerminalLifecycleObserver
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val REQUEST_CODE_LOCATION = 123
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        validarPermisos()
         if (usuarioLogeado())
             redirigirAMenu()
     }
@@ -42,21 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     fun establecerPantallaCompleta() {
         window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+            (View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
         supportActionBar?.hide()
-    }
-    //Permisos de Localización. Necesarios para el SDK de Stripe.
-    private fun validarPermisos() {
-        if (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_LOCATION)
-        }
-    }
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_LOCATION && grantResults.isNotEmpty()
-            && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-            throw RuntimeException("Location services are required in order to " + "connect to a reader.")
-        }
     }
 }
--- app/src/main/java/com/entrecanos/loteria/PerfilActivity.kt
+++ app/src/main/java/com/entrecanos/loteria/PerfilActivity.kt
@@ -1,16 +1,149 @@
 package com.entrecanos.loteria
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.entrecanos.loteria.control.EphemeralKeyProviderImplementation
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.MetadataChanges
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.stripe.android.*
+import com.stripe.android.model.ConfirmPaymentIntentParams
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.view.BillingAddressFields
+import kotlinx.android.synthetic.main.activity_perfil.*
 
 class PerfilActivity : AppCompatActivity() {
+    private var currentUser: FirebaseUser = FirebaseAuth.getInstance().currentUser!!
+    private lateinit var paymentSession: PaymentSession
+    private lateinit var selectedPaymentMethod: PaymentMethod
+    private val stripe: Stripe by lazy {
+        Stripe(
+            applicationContext,
+            PaymentConfiguration.getInstance(applicationContext).publishableKey
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+        setupPaymentSession()
+        paymentmethod.setOnClickListener {
+            paymentSession.presentPaymentMethodSelection()
+            Log.d("Payment", "Click")
+        }
+        payButton.setOnClickListener {
+            confirmPayment(selectedPaymentMethod.id!!)
+        }
+
+    }
+
+    private fun setupPaymentSession() {
+        // Setup Customer Session
+        PaymentConfiguration.init(
+            this,
+            "pk_test_51HJOakIwHoErs7JvBCqywtbREZD7P1kznU4N0S8Hmz1nJXilXUFIy63Egbi6gHz0rg0Eak6jwQ2u7o1RX9o0XvTb00vSjfblaS"
+        )
+        CustomerSession.initCustomerSession(this, EphemeralKeyProviderImplementation())
+        // Setup a payment session
+        paymentSession = PaymentSession(
+            this, PaymentSessionConfig.Builder()
+                .setShippingInfoRequired(false)
+                .setShippingMethodsRequired(false)
+                .setBillingAddressFields(BillingAddressFields.None)
+                .setShouldShowGooglePay(true)
+                .build()
+        )
+
+        paymentSession.init(
+            object : PaymentSession.PaymentSessionListener {
+                override fun onPaymentSessionDataChanged(data: PaymentSessionData) {
+                    Log.d("PaymentSession", "PaymentSession has changed: $data")
+                    Log.d("PaymentSession", "${data.isPaymentReadyToCharge} <> ${data.paymentMethod}")
+
+                    if (data.isPaymentReadyToCharge) {
+                        Log.d("PaymentSession", "Ready to charge")
+                        payButton.isEnabled = true
+
+                        data.paymentMethod?.let {
+                            Log.d("PaymentSession", "PaymentMethod $it selected")
+                            paymentmethod.text = "${it.card?.brand} card ends with ${it.card?.last4}"
+                            selectedPaymentMethod = it
+                        }
+                    }
+                }
+
+                override fun onCommunicatingStateChanged(isCommunicating: Boolean) {
+                    Log.d("PaymentSession", "isCommunicating $isCommunicating")
+                }
+
+                override fun onError(errorCode: Int, errorMessage: String) {
+                    Log.e("PaymentSession", "onError: $errorCode, $errorMessage")
+                }
+            }
+        )
+
+    }
+
+    private fun confirmPayment(paymentMethodId: String) {
+        payButton.isEnabled = false
+
+        val paymentCollection = Firebase.firestore
+            .collection("stripe_customers").document(currentUser.uid)
+            .collection("payments")
+
+        // Add a new document with a generated ID
+        paymentCollection.add(
+            hashMapOf(
+                "amount" to 5000,
+                "currency" to "mxn"
+            )
+        )
+            .addOnSuccessListener { documentReference ->
+                Log.d("payment", "DocumentSnapshot added with ID: ${documentReference.id}")
+                documentReference.addSnapshotListener(MetadataChanges.INCLUDE) { snapshot, e ->
+                    if (e != null) {
+                        Log.w("payment", "Listen failed.", e)
+                        return@addSnapshotListener
+                    }
+
+                    if (snapshot != null && snapshot.exists()) {
+                        Log.d("payment", "Current data: ${snapshot.data}")
+                        val clientSecret = snapshot.getString("client_secret")
+                        if(clientSecret == null) return@addSnapshotListener
+                        Log.d("payment", "Create paymentIntent returns $clientSecret")
+                        clientSecret.let {
+                            stripe.confirmPayment(
+                                this, ConfirmPaymentIntentParams.createWithPaymentMethodId(
+                                    paymentMethodId,
+                                    (it as String)
+                                )
+                            )
+
+                            Toast.makeText(applicationContext, "Payment Done!!", Toast.LENGTH_LONG).show()
+                            Log.e("payment", "AL FIN CARAJAMADRE")
+                        }
+                    } else {
+                        Log.e("payment", "Current payment intent : null")
+                        payButton.isEnabled = true
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("payment", "Error adding document", e)
+                payButton.isEnabled = true
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        paymentSession.handlePaymentData(requestCode, resultCode, data ?: Intent())
     }
 
     //Código de Menu
@@ -18,11 +151,12 @@ class PerfilActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.nav_menu_perfil, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_sign_out -> {
                 FirebaseAuth.getInstance().signOut()
-                val intent = Intent(this, PerfilActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(intent)
             }
